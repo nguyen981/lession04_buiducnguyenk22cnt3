3555e5fdf55e49c9f9c52e181d9e6c3e945948c6
nguyen981 <66893954+nguyen981@users.noreply.github.com>
2024-05-15 23:56:38 +0700

    Add files via upload

---
+++ Controllers/BdnCustomerController.cs
@@ -0,0 +1,58 @@
+﻿using Bdn_Lesson04.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bdn_Lesson04.Controllers
+{
+    public class BdnCustomerController : Controller
+    {
+        // GET: BdnCustomer
+        public ActionResult Index()
+        {
+            return View();
+            
+        }
+        // Action BdnCustomerDetail 
+        public ActionResult BdnCustomerDetail()
+        {
+            // tao doi tuong du lieu 
+            var customer = new BdnCustomer ()
+            {
+
+            }
+            return View();
+        }
+        // action: BdnCustomerDetail
+        public ActionResult BdnCustomerDetail()
+        {
+            var customer = new Bdncustomer()
+            {
+                CustomerId = 1,
+                FirstName = "Bui Duc",
+                Lastname = " Nguyen",
+                Address = "K22CNT3",
+                YearofBirth = 2004
+
+            };
+            ViewBag.customer = customer;
+            return View();
+        }
+
+    public ActionResult BdnListCustomer()
+        {
+            var list = new List<BdnCustomer>()
+            {
+                new Bdncustomer(){
+                CustomerId = 1,
+                FirstName = "Bui Duc",
+                Lastname = " Nguyen",
+                Address = "K22CNT3",
+                YearofBirth = 2004
+                }
+            };
+        }
+    }
+}
---
+++ Models/Bdncustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bdn_Lesson04.Models
+{
+    public class Bdncustomer
+    {
+        public int CustomerId { get; set; }
+        public string Firtname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public int YearofBirth { get; set; }
+    }
+}
